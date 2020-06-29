99ba1172bcc2d74b14ef5ebeb3ae3e651c5a7a29
neuecc <ils@neue.cc>
2020-06-29 17:10:36 +0900

    unity

--- src/ZString.Unity/Assets/Scripts/Editor/PackageExporter.cs
+++ src/ZString.Unity/Assets/Scripts/Editor/PackageExporter.cs
@@ -9,10 +9,9 @@ public static class PackageExporter
     [MenuItem("Tools/Export Unitypackage")]
     public static void Export()
     {
-        var version = Environment.GetEnvironmentVariable("UNITY_PACKAGE_VERSION");
-
-        // configure
         var root = "Scripts/ZString";
+        var version = GetVersion(root);
+
         var fileName = string.IsNullOrEmpty(version) ? "ZString.Unity.unitypackage" : $"ZString.Unity.{version}.unitypackage";
         var exportPath = "./" + fileName;
 
@@ -40,4 +39,41 @@ public static class PackageExporter
 
         UnityEngine.Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
     }
+
+    static string GetVersion(string root)
+    {
+        var version = Environment.GetEnvironmentVariable("UNITY_PACKAGE_VERSION");
+        var versionJson = Path.Combine(Application.dataPath, root, "package.json");
+
+        if (File.Exists(versionJson))
+        {
+            var v = JsonUtility.FromJson<Version>(File.ReadAllText(versionJson));
+
+            if (!string.IsNullOrEmpty(version))
+            {
+                if (v.version != version)
+                {
+                    var msg = $"package.json and env version are mismatched. UNITY_PACKAGE_VERSION:{version}, package.json:{v.version}";
+
+                    if (Application.isBatchMode)
+                    {
+                        Console.WriteLine(msg);
+                        Application.Quit(1);
+                    }
+
+                    throw new Exception("package.json and env version are mismatched.");
+                }
+            }
+
+            version = v.version;
+        }
+
+        return version;
+    }
+
+    public class Version
+    {
+        public string version;
+    }
+
 }
