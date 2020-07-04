6b153146b0e644cd43f25634b30451cc6d639c34
udaken <uda.ken+git@gmail.com>
2020-07-04 09:19:05 +0900

    Fix NetCore2Tests

--- tests/ZString.Tests/RemoveTest.cs
+++ tests/ZString.Tests/RemoveTest.cs
@@ -11,43 +11,49 @@ namespace ZStringTests
         public void RemovePart()
         {
             string str = "The quick brown fox jumps over the lazy dog.";
-            using var zsb = ZString.CreateStringBuilder();
-            zsb.Append(str);
-            var bcl = new StringBuilder(str);
+            using (var zsb = ZString.CreateStringBuilder())
+            {
+                zsb.Append(str);
+                var bcl = new StringBuilder(str);
 
-            // Remove "brown "
-            zsb.Remove(10, 6);
-            bcl.Remove(10, 6);
+                // Remove "brown "
+                zsb.Remove(10, 6);
+                bcl.Remove(10, 6);
 
-            zsb.ToString().Should().Be(bcl.ToString());
+                zsb.ToString().Should().Be(bcl.ToString());
+            }
         }
 
         [Fact]
         public void RemoveAll()
         {
             string str = "The quick brown fox jumps over the lazy dog.";
-            using var zsb = ZString.CreateStringBuilder();
-            zsb.Append(str);
-            var bcl = new StringBuilder(str);
+            using (var zsb = ZString.CreateStringBuilder())
+            {
+                zsb.Append(str);
+                var bcl = new StringBuilder(str);
 
-            zsb.Remove(0, str.Length);
-            bcl.Remove(0, str.Length);
+                zsb.Remove(0, str.Length);
+                bcl.Remove(0, str.Length);
 
-            zsb.ToString().Should().Be(bcl.ToString());
+                zsb.ToString().Should().Be(bcl.ToString());
+            }
         }
 
         [Fact]
         public void RemoveTail()
         {
             string str = "foo,bar,baz";
-            using var zsb = ZString.CreateStringBuilder();
-            zsb.Append(str);
-            var bcl = new StringBuilder(str);
+            using (var zsb = ZString.CreateStringBuilder())
+            {
+                zsb.Append(str);
+                var bcl = new StringBuilder(str);
 
-            zsb.Remove(7, 4);
-            bcl.Remove(7, 4);
+                zsb.Remove(7, 4);
+                bcl.Remove(7, 4);
 
-            zsb.ToString().Should().Be(bcl.ToString());
+                zsb.ToString().Should().Be(bcl.ToString());
+            }
         }
     }
 }
